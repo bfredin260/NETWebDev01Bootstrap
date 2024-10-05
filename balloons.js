a62e674d8c2281764337160a9cc68ce6e7250ddd
Braedon Fredin <b260fredin@gmail.com>
2024-10-04 21:21:07 -0500

    [3.10] Randomized h1 attention seeker on page load

--- balloons.js
+++ balloons.js
@@ -1,4 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const random = Math.floor(Math.random() * 12) + 1;
+    let animation;
+    switch(random) {
+        case 1:
+            animation = 'animate__bounce';
+            break;
+        case 2:
+            animation = 'animate__flash';
+            break;
+        case 3:
+            animation = 'animate__pulse';
+            break;
+        case 4:
+            animation = 'animate__rubberBand';
+            break;
+        case 5:
+            animation = 'animate__shakeX';
+            break;
+        case 6:
+            animation = 'animate__shakeY';
+            break;
+        case 7:
+            animation = 'animate__headShake';
+            break;
+        case 8:
+            animation = 'animate__swing';
+            break;
+        case 9:
+            animation = 'animate__tada';
+            break;
+        case 10:
+            animation = 'animate__wobble';
+            break;
+        case 11:
+            animation = 'animate__jello';
+            break;
+        default:
+            animation = 'animate__heartBeat';
+            break;
+    }
+    document.getElementsByTagName('h1')[0].classList.add(animation);
+    
     const elem = document.getElementById('dob');
     const datepicker = new Datepicker(elem, {
       // options
